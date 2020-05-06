23e401b59bf2d84b5ea5e26141c0ad03f3e4669e
Pavan <rogerio_pavan@hotmail.com>
2020-05-06 00:17:03 -0300

    add cross-env

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import dotenv from 'dotenv';
 import app from './app';
 import { resolve } from 'path';
 
+const http = require('http');
+
+const server = http.createServer(app);
+
 dotenv.config({ path: resolve('env', `.env.${process.env.NODE_ENV}`) });
 
-app.listen(process.env.APP_PORT, () => {
+server.listen(process.env.APP_PORT, () => {
   console.log(`Application running on port ${process.env.APP_PORT}`);
 });
